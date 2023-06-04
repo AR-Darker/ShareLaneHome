b58db368e196144e3ab9e4a7567e7d6d9b3970e3
Ivan <ishkredov2@gmail.com>
2023-06-05 02:50:58 +0300

    Initial commit

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void zipCodeShouldNotAccept4Digits(){
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("1234");
+
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
+
+        WebElement errorMessage = driver.findElement((By.cssSelector("[class='error_message']")));
+        boolean isDisplayed = errorMessage.isDisplayed();
+
+        String errorMessageForEquals = driver.findElement(By.cssSelector("[class='error_message']")).getText();
+
+        Assert.assertTrue(isDisplayed, "Текст errormessage НеПолучен");
+
+        Assert.assertEquals(errorMessageForEquals,"Oops, error on page. ZIP code should have 5 digits",
+                "ErrorMessage is notcorrect");
+
+        driver.quit();
+    }
+}
+
+
